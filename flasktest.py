511636d9ff0233601b9fc8d640ba66f9a8130a4e
jw0113 <jiwoo5430@gmail.com>
2021-10-08 05:51:27 +0900

    python response fail

--- flasktest.py
+++ flasktest.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import urllib.request
+
 
 app = Flask(__name__)
 
-@app.route("/flasktest")
+@app.route("/flasktest", methods = ['GET', 'POST'])
 def flasktest():
-    return "flasktest"
+    if(request.method == 'GET'):
+        header = request.headers.get("Content-Type")
+        f = request.files['file']
+        print(f)
+        print(header)
+        return "response {} ".format(f)
 
 if __name__ == '__main__':
     app.run(debug=False, host="127.0.0.1",port=5000)
