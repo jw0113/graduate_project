1308b1253e9ea3ed0224d8d4abf33b342865738a
jw0113 <jiwoo5430@gmail.com>
2021-10-08 04:54:27 +0900

    spring to python send success

--- GraudateProject/src/main/java/com/spring/graduateproject/FileService.java
+++ GraudateProject/src/main/java/com/spring/graduateproject/FileService.java
@@ -1,16 +1,15 @@
 package com.spring.graduateproject;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.UUID;
 
-import javax.net.ssl.HttpsURLConnection;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,9 +28,10 @@ public class FileService implements IFileService {
 			// boundary값으로 사용하기 위한 무작위 값
 			UUID uuid = UUID.randomUUID();
 			
-			URL url = new URL(null,"http://127.0.0.1:5000/flasktest", new sun.net.www.protocol.https.Handler());
+			URL url = new URL("http://127.0.0.1:5000/flasktest");
 			// HttpURLConnection 객체를 생성하여 URL 연결
-			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			System.out.println("연동완료");
 			
 			// Request Header 설정
 			con.setRequestProperty("Content-Type", "multipart/form-data;charset=utf-8;boundary=" + uuid);
@@ -48,16 +48,33 @@ public class FileService implements IFileService {
 			// 바이트 기반 스트림에서 출력 스트림으로 변환시켜 저장
 			PrintWriter w = new PrintWriter(new OutputStreamWriter(outputstream, "UTF-8"), true);
 			
+			System.out.println("확인");
+			
 			// 파일 데이터를 추가한다.
 			w.append("--" + uuid).append(LINE_D);
-			w.append("Content-Dispotion; form-data; name=\"file\"; filename=\"").append(LINE_D);
+			w.append("Content-Dispotion; form-data; name=\"file\"; filename=\"" + f.getName() + "\"").append(LINE_D);
+			w.append("Content-Transfer-Encoding: binary").append(LINE_D);
+			w.append(LINE_D);
+			w.flush();
 			
+			// 파일 데이터 구하기
+			byte[] fileData = f.getBytes();
+			outputstream.write(fileData);
 			
+			outputstream.flush();
+			w.append(LINE_D);
+			w.flush();
 			
+			w.append("--" + uuid + "--");
+			w.close();
 			
+			// Request 요청 후 응답 코드 받기
+			int responseCode = con.getResponseCode();
 			
-			
-			
+			if (responseCode == 200) {
+				System.out.println("전송 성공 Success!!!!!!!!!!!");
+			}
+				
 			
 			
 			
