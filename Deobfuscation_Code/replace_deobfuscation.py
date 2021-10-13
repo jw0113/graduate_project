1bfdbb0cf966b07bb51f86b180463c78cb0752a0
jw0113 <jiwoo5430@gmail.com>
2021-10-14 02:37:46 +0900

    replace_deobfuscation update 1014

---
+++ Deobfuscation_Code/replace_deobfuscation.py
@@ -0,0 +1,13 @@
+# powershell - replace() 해제 코드
+
+
+
+def replace_deob(deobfuscation) :
+
+    # deobfuscation은 ""이로 인해 오류 발생 가능하므로 여기로 넘어오기 전 ""을 ''으로 바꿔야함
+
+    #re_str = "'z(dldm)OP://192/168/1/144/z(dldm)OP'.replace('z(dldm)OP','http')"
+    re_str = deobfuscation
+    re_deob = eval(re_str)
+    
+    print("replace deobfuscation : " + re_deob)
