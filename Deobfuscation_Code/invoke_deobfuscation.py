f9c1fef774dea2b6e9fb61939f857f4766eb12f9
jw0113 <jiwoo5430@gmail.com>
2021-11-04 23:58:10 +0900

    탐지&해제 update success 1104

--- Deobfuscation_Code/invoke_deobfuscation.py
+++ Deobfuscation_Code/invoke_deobfuscation.py
@@ -5,5 +5,5 @@ import os
 def b_deobfuscation(deobfuscation) :
     
     # site 추천 문자열 전송
-    result = "about InvokeExpression, we suggest already using code.\n"
+    result = "about InvokeExpression, we suggest already using code."
     return result
